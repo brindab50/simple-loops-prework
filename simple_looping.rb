f36b77f2ed6ef2148c84b7aafa8384d179e40f3a
brindab50 behal <brindab50@gmail.com>
2019-03-05 13:20:01 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,6 +8,7 @@ def loop_iterator(number_of_times)
     puts "Welcome to Flatiron School's Web Development Course!"
     loop +=1
   end
+end
 
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
