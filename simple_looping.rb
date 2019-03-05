459db35e5d12dae3e9595b22df122ca34e278655
brindab50 behal <brindab50@gmail.com>
2019-03-05 13:20:23 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,10 +4,11 @@ def loop_iterator(number_of_times)
   loop = 0 
   7.times do 
     puts "Welcome to Flatiron School's Web Development Course!"
+  end
     while loop < 7
     puts "Welcome to Flatiron School's Web Development Course!"
     loop +=1
-  end
+  
 end
 
   # Code your solution here using the "loop" keyword to puts out the below phrase
