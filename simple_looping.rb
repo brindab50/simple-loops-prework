8df8ea274db348391e06adcc92ef94b3113b67c3
brindab50 behal <brindab50@gmail.com>
2019-03-05 13:16:45 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,7 +2,7 @@
 
 def loop_iterator(number_of_times)
   loop = 0 
-  
+  7.times do 
 
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
